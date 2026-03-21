fec2248767df5dadb917c63541c4866762a84ae8
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-03-21 23:46:17 +0900

    change handless option for github action

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { chromium } = require("playwright");
 const fs = require("fs");
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
   console.log(1);
   page.on("response", async (res) => {
